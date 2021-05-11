09a1892bed4fd3937aae146c04631ef30b4fc93c
mohammed mustafa <mohammed.alkhddar@gmail.com>
2021-05-10 20:00:53 -0500

    context

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,10 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 //caling function type ion type script :) 
+import { TodosContext } from '../store/todos-context';
 import classes from './NewTodo.module.css';
 
-const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = (props) => {
  
-
+    const todosCtx = useContext(TodosContext);
     const todoTextInputRef = useRef<HTMLInputElement>(null);
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
@@ -16,7 +17,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
           return;
         }
     
-        props.onAddTodo(enteredText);
+        todosCtx.addTodo(enteredText);
       };
 
 
